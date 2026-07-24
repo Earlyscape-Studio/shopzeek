bf35d9da90967fdb926be06b8884332ed508a8e2
hill onyechekwa <hillonyechekwa@gmail.com>
2026-07-24 17:06:08 +0100

    Update email templates to use new logo URL

--- components/emails/abandonedCartEmail.tsx
+++ components/emails/abandonedCartEmail.tsx
@@ -39,7 +39,7 @@ export const AbandonedCartEmail = ({
                 alt="zeek logo"
                 width={250}
                 height={150}
-                src={`${baseUrl}/static/zeek1.svg`} 
+                src="https://swjqqxhvicbxqcembkml.supabase.co/storage/v1/object/sign/email%20assets/zeek1.svg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV85ZmMyMThlOS1mZmMzLTQxMTItODlkYy02M2QxNmRkOTU0ZjMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJlbWFpbCBhc3NldHMvemVlazEuc3ZnIiwic2NvcGUiOiJkb3dubG9hZCIsImlhdCI6MTc4NDkwNzEzNCwiZXhwIjoxODE2NDQzMTM0fQ.tRzoJ4BJbBGid_qp8QsDaw4rRP_mPLcmDesV9rYD5fo"
                 />
             </Tailwind>
           </Heading>
